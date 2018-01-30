4edca4b8e50492e2c3f435dba1913d7a77327a65
jollus88 <joel@joeldoesdigital.com>
2018-01-31 10:27:01 +1100

    Updating G&W modifiers.

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 // use a cacheName for cache versioning
-var cacheName = ['v9_5:static'];
+var cacheName = ['v9_6:static'];
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
