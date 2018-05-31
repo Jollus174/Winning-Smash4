88b13e238f6fba2c890a63a824c63400586dcf13
JoelS <joel@joeldoesdigital.com>
2018-05-31 21:54:51 +1000

    all set for staging builds

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 // use a cacheName for cache versioning
-var cacheName = ['v2_3:static'];
+var cacheName = ['v2_42:static'];
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
