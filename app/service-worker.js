57f2b634a6e788129c78a5654b9a2867bde5f9df
JoelS <joel@joeldoesdigital.com>
2018-06-05 23:42:03 +1000

    all ready for merge to master

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,5 @@
 // use a cacheName for cache versioning
-var cacheName = ['v2_46:static'];
+var cacheName = ['v2_47:static'];
 
 // during the install phase you usually want to cache static assets
 self.addEventListener('install', function(e) {
